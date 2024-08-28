eb86fa94b9cb326e2551fd5a1a4fc4837d974ffd
khayri <khayriramzy@gmail.com>
2024-08-28 05:17:18 -0400

    Create if statement to return 'ERROR' if a negative number is entered

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,8 @@
 const repeatString = function(string, number) {
+    // if negative number is entered, return ERROR
+    if (number < 0){
+        return 'ERROR';
+    }
     // create variable 'repeatedWord' to store the string
     let repeatedWord = '';
     // create for loop that concatenates a string to 'i' variable a certain number of times
