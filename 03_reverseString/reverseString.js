d4dcf900e435d9a59fdd649901b4169cfaf51632
khayri <khayriramzy@gmail.com>
2024-08-28 09:01:34 -0400

    Fill in pseudocode with actual code

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,11 +1,19 @@
 const reverseString = function(word) {
     // create reversedString variable to store reversed string
+    let reversedString = [];
     // turn word into array using split()
+    const myArray = word.split('');
     // create for loop 
-    // create 'lastInArray' variable and assign pop() to it
-    // concatenate lastInArray to reversedString
-    // after for loop is over, return reversedString
+    for (const letter of word){
+        // create 'lastInArray' variable and assign pop() to it
+        let lastInArray = word.pop();
+        // concatenate lastInArray to reversedString
+        reversedString += lastInArray;
 
+    }
+    
+    // after for loop is over, return reversedString
+    return reversedString;
 };
 
 // Do not edit below this line
