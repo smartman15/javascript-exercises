fbcd0fbb9226be512bc3febf8be8c630565566c5
khayri <khayriramzy@gmail.com>
2024-08-28 04:34:12 -0400

    Add variables for function to take

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,4 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
     // create variable 'i' to store the string
     // create for loop that concatenates a string to 'i' variable a certain number of times
     // print 'i'
