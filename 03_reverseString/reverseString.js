22800ff30b999b5d2381381daaa9c754d0a10dbb
khayri <khayriramzy@gmail.com>
2024-08-28 08:49:08 -0400

    Create pseudocode

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
 const reverseString = function(word) {
-    
+    // create reversedString variable to store reversed string
+    // turn word into array using split()
+    // create for loop 
+    // create 'lastInArray' variable and assign pop() to it
+    // concatenate lastInArray to reversedString
+    // after for loop is over, return reversedString
+
 };
 
 // Do not edit below this line
