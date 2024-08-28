9190b5f7fd957fa3481cda169128374bc86da925
khayri <khayriramzy@gmail.com>
2024-08-28 04:44:17 -0400

    Change i <= number to i < number, add a console.log()

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -2,11 +2,15 @@ const repeatString = function(string, number) {
     // create variable 'repeatedWord' to store the string
     let repeatedWord = '';
     // create for loop that concatenates a string to 'i' variable a certain number of times
-    for (let i = 0; i <= number; i++){
+    for (let i = 0; i < number; i++){
         // concatenate string to repeatedWord
         repeatedWord += string;
     }
+    // alert repeatedWord
+    console.log(repeatedWord);
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
