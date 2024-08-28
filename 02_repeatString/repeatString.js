1bf018996beaf7cf92b260e30a55490e597cce43
khayri <khayriramzy@gmail.com>
2024-08-28 04:33:37 -0400

    Create pseudocode

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,7 @@
 const repeatString = function() {
-
+    // create variable 'i' to store the string
+    // create for loop that concatenates a string to 'i' variable a certain number of times
+    // print 'i'
 };
 
 // Do not edit below this line
