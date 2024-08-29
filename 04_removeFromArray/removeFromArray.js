e8c5e2836327badb90031de7a39cd4047445ef9c
khayri <khayriramzy@gmail.com>
2024-08-29 04:23:10 -0400

    Create for loop

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,10 @@
 const removeFromArray = function(array, deleteElement) {
     // create for loop 
-    // keep iterating until it finds element that you want to remove
-    // after finding the element, use slice() to remove it from array
+    for (i = 0 ; i < array.length ; i++){
+        // keep iterating until it finds element that you want to remove
+        // after finding the element, use slice() to remove it from array
+
+    }
     // after for loop is over, return array from slice() result
 };
 
