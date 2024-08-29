23a9e1bd1fa315f6593b6e2512a9d5ec3fb5e696
khayri <khayriramzy@gmail.com>
2024-08-29 10:01:08 -0400

    Change i < array.length to i <= array.length in for loop parentheses

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,6 @@
 const removeFromArray = function(array, deletedElement1, deletedElement2, deletedElement3, deletedElement4) {
     // create for loop 
-    for (let i = 0 ; i < array.length ; i++){
+    for (let i = 0 ; i <= array.length ; i++){
         // reset i to 0
         i = 0;
         // keep iterating until it finds element that you want to remove (use if statement)
