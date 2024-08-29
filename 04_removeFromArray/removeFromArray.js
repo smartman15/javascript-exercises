6cd5177de23a711840be6a5a895daa11e1ef0645
khayri <khayriramzy@gmail.com>
2024-08-29 05:08:30 -0400

    Create another function parameter 'deletedElement2' to remove multiple elements from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,8 @@
-const removeFromArray = function(array, deletedElement) {
+const removeFromArray = function(array, deletedElement1, deletedElement2) {
     // create for loop 
     for (i = 0 ; i < array.length ; i++){
         // keep iterating until it finds element that you want to remove (use if statement)
-        if (array[i] === deletedElement){
+        if (array[i] === deletedElement1 || array[i] === deletedElement2){
             // after finding the element, use splice() to remove it from array
             resultArray = array.splice(i, 1);
         }
