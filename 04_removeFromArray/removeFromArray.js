30869a4df44507b909af6130f6facb3ad31c030c
khayri <khayriramzy@gmail.com>
2024-08-29 05:34:26 -0400

    Remove resultArray as it was unnecessary (i think)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -4,12 +4,15 @@ const removeFromArray = function(array, deletedElement1, deletedElement2) {
         // keep iterating until it finds element that you want to remove (use if statement)
         if (array[i] === deletedElement1 || array[i] === deletedElement2){
             // after finding the element, use splice() to remove it from array
-            resultArray = array.splice(i, 1);
+            array.splice(i, 1);
         }
     }
     // after for loop is over, return array from splice() result
     return array;
 };
+
+let array = [1,2,3,4];
+console.log(removeFromArray(array, 4, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
