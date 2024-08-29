08deca45c168a7b3dea504d681a70c5dafb0a48d
khayri <khayriramzy@gmail.com>
2024-08-29 04:28:33 -0400

    Create if statement, replace slice() with splice(), and rename function parameter

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,11 @@
-const removeFromArray = function(array, deleteElement) {
+const removeFromArray = function(array, deletedElement) {
     // create for loop 
     for (i = 0 ; i < array.length ; i++){
-        // keep iterating until it finds element that you want to remove
-        // after finding the element, use slice() to remove it from array
+        // keep iterating until it finds element that you want to remove (use if statement)
+        if (array[i] === deletedElement){
+            // after finding the element, use splice() to remove it from array
+            resultArray = array.splice()
+        }
 
     }
     // after for loop is over, return array from slice() result
