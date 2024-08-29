7fbfcb757196f2611b2b6543b8baa8c24f18da10
khayri <khayriramzy@gmail.com>
2024-08-29 09:37:09 -0400

    Add let next to i in for loop

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,8 @@
 const removeFromArray = function(array, deletedElement1, deletedElement2, deletedElement3, deletedElement4) {
     // create for loop 
-    for (i = 0 ; i < array.length ; i++){
+    for (let i = 0 ; i < array.length ; i++){
         // keep iterating until it finds element that you want to remove (use if statement)
-        if (array[i] === deletedElement1 || array[i] === deletedElement2){
+        if (array[i] === deletedElement1 || array[i] === deletedElement2 || array[i] === deletedElement3 || array[i] === deletedElement4){
             // after finding the element, use splice() to remove it from array
             array.splice(i, 1);
             i = 0;
@@ -12,8 +12,6 @@ const removeFromArray = function(array, deletedElement1, deletedElement2, delete
     return array;
 };
 
-let array = [1,2,3,4];
-console.log(removeFromArray(array, 3, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
