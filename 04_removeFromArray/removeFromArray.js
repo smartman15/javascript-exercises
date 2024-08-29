82aa44156af908a7997aa82c6480314740141099
khayri <khayriramzy@gmail.com>
2024-08-29 09:50:33 -0400

    Move i = 0 at the beginning of for loop instead of end of if statement

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,12 @@
 const removeFromArray = function(array, deletedElement1, deletedElement2, deletedElement3, deletedElement4) {
     // create for loop 
     for (let i = 0 ; i < array.length ; i++){
+        // reset i to 0
+        i = 0;
         // keep iterating until it finds element that you want to remove (use if statement)
         if (array[i] === deletedElement1 || array[i] === deletedElement2 || array[i] === deletedElement3 || array[i] === deletedElement4){
             // after finding the element, use splice() to remove it from array
             array.splice(i, 1);
-            i = 0;
         }
     }
     // after for loop is over, return array from splice() result
