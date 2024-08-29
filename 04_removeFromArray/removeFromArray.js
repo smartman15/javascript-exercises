7e87f1df28d6f8ceabb49aaed44b9df8d026f7db
khayri <khayriramzy@gmail.com>
2024-08-29 04:20:40 -0400

    Create pseudocode

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
 const removeFromArray = function(array) {
+    // create for loop 
+    // keep iterating until it finds element that you want to remove
+    // after finding the element, use slice() to remove it from array
+    // after for loop is over, return array from slice() result
 };
 
 // Do not edit below this line
