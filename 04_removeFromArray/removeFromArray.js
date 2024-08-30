c982213b0f2ff146f961c09ad13b944bee5a3b91
khayri <khayriramzy@gmail.com>
2024-08-30 11:30:59 -0400

    Remove i = 0 at the beginning of loop, change <= array.length to < array.length
    Testing some stuff since for some reason setting i = 0 at the beginning of the loop makes the debugger freeze (probably because of it creating an infinite loop)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,12 @@
 const removeFromArray = function(array, deletedElement1, deletedElement2, deletedElement3, deletedElement4) {
     // create for loop 
-    for (let i = 0 ; i <= array.length ; i++){
-        // reset i to 0
-        i = 0;
+    for (let i = 0 ; i < array.length ; i){
+        // check if array is empty
+        if (array.length === 0){
+            // if empty, return array
+            return array;
+        }
+        
         // keep iterating until it finds element that you want to remove (use if statement)
         if (array[i] === deletedElement1 || array[i] === deletedElement2 || array[i] === deletedElement3 || array[i] === deletedElement4){
             // after finding the element, use splice() to remove it from array
