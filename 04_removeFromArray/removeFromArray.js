6c9c34f4cead6b2b2b3e557e44a23d60755bcb49
khayri <khayriramzy@gmail.com>
2024-09-01 03:10:44 -0400

    Create if statements for deletedElement

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -7,7 +7,24 @@ const removeFromArray = function(array, deletedElement1, deletedElement2, delete
         // keep iterating until it finds element that you want to remove (use if statement)
         if (element === deletedElement1 || element === deletedElement2 || element === deletedElement3 || element === deletedElement4){
             // after finding the element, use splice() to remove it from array
-            array.splice()
+
+            // if deletedElement1
+            if (element === deletedElement1){
+                array.splice(index, 1, deletedElement1);
+            }
+            // if deletedElement2
+            else if (element === deletedElement2){
+                array.splice(index, 1, deletedElement2);
+            }
+            // if deletedElement3
+            else if (element === deletedElement3){
+                array.splice(index, 1, deletedElement3);
+            }
+            // if deletedElement4
+            else if (element === deletedElement4){
+                array.splice(index, 1, deletedElement4);
+            }
+            
 
         }
             
