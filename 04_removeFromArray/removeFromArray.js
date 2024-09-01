49c7df72b57cae0f95480234a602cbb1ea5f4c5f
khayri <khayriramzy@gmail.com>
2024-09-01 02:52:45 -0400

    Remove code and rewrite for loop

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,20 +1,11 @@
 const removeFromArray = function(array, deletedElement1, deletedElement2, deletedElement3, deletedElement4) {
     // create for loop 
-    for (let i = 0 ; i < array.length ; i){
-        // check if array is empty
-        if (array.length === 0){
-            // if empty, return array
-            return array;
-        }
+    for (const element of array){
         
         // keep iterating until it finds element that you want to remove (use if statement)
-        if (array[i] === deletedElement1 || array[i] === deletedElement2 || array[i] === deletedElement3 || array[i] === deletedElement4){
             // after finding the element, use splice() to remove it from array
-            array.splice(i, 1);
-        }
-    }
+        
     // after for loop is over, return array from splice() result
-    return array;
 };
 
 
