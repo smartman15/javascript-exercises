f80475ee8de435701e6abce4b044b36a1f427f7d
khayri <khayriramzy@gmail.com>
2024-09-01 03:02:15 -0400

    Create index variable and assign it 0

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,13 @@
 const removeFromArray = function(array, deletedElement1, deletedElement2, deletedElement3, deletedElement4) {
     // create for loop 
     for (const element of array){
+        // create index from 0
+        let index = 0;
         
         // keep iterating until it finds element that you want to remove (use if statement)
         if (element === deletedElement1 || element === deletedElement2 || element === deletedElement3 || element === deletedElement4){
             // after finding the element, use splice() to remove it from array
-            
+            array.splice()
 
         }
             
