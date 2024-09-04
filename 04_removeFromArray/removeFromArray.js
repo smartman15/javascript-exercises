5c31f03f9b5eddb5219097dcfc2ace085691b892
khayri <khayriramzy@gmail.com>
2024-09-04 11:15:06 -0400

    Revert code to earlier version and run second test of spec file
    Hopefully this way all the problems can be solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,35 +1,15 @@
 const removeFromArray = function(array, deletedElement1, deletedElement2, deletedElement3, deletedElement4) {
     // create for loop 
-    for (const element of array){
-        // create index from 0
-        let index = 0;
+    for (let i = 0 ; i < array.length ; i++){
         
         // keep iterating until it finds element that you want to remove (use if statement)
-        if (element === deletedElement1 || element === deletedElement2 || element === deletedElement3 || element === deletedElement4){
+        if (array[i] === deletedElement1 || array[i] === deletedElement2 || array[i] === deletedElement3 || array[i] === deletedElement4){
             // after finding the element, use splice() to remove it from array
-
-            // if deletedElement1
-            if (element === deletedElement1){
-                array.splice(index, 1, deletedElement1);
-            }
-            // if deletedElement2
-            else if (element === deletedElement2){
-                array.splice(index, 1, deletedElement2);
-            }
-            // if deletedElement3
-            else if (element === deletedElement3){
-                array.splice(index, 1, deletedElement3);
-            }
-            // if deletedElement4
-            else if (element === deletedElement4){
-                array.splice(index, 1, deletedElement4);
-            }
-            
-
+            array.splice(i, 1);
         }
-            
-
+    }
     // after for loop is over, return array from splice() result
+    return array;
 };
 
 
--- 04_removeFromArray/removeFromArray.spec.js
+++ 04_removeFromArray/removeFromArray.spec.js
@@ -1,19 +1,19 @@
 const removeFromArray = require('./removeFromArray')
 
 describe('removeFromArray', () => {
-  test('removes a single value', () => {
+  test.skip('removes a single value', () => {
     expect(removeFromArray([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
   });
   test('removes multiple values', () => {
     expect(removeFromArray([1, 2, 3, 4], 3, 2)).toEqual([1, 4]);
   });
-  test('removes multiple of the same value', () => {
+  test.skip('removes multiple of the same value', () => {
     expect(removeFromArray([1, 2, 2, 3], 2)).toEqual([1, 3]);
   });
-  test('ignores non present values', () => {
+  test.skip('ignores non present values', () => {
     expect(removeFromArray([1, 2, 3, 4], 7, "tacos")).toEqual([1, 2, 3, 4]);
   });
-  test('ignores non present values, but still works', () => {
+  test.skip('ignores non present values, but still works', () => {
     expect(removeFromArray([1, 2, 3, 4], 7, 2)).toEqual([1, 3, 4]);
   });
   test.skip('can remove all values', () => {
