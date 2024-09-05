781fa84daa31c3703c2f9277481d8c37ca77144b
khayri <khayriramzy@gmail.com>
2024-09-05 11:13:13 -0400

    Change i++ to i in for loop parentheses, reset index to 0 after splice, add else statement

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,16 @@
 const removeFromArray = function(array, deletedElement1, deletedElement2, deletedElement3, deletedElement4) {
     // create for loop 
-    for (let i = 0 ; i < array.length ; i++){
+    for (let i = 0 ; i < array.length ; i){
         
         // keep iterating until it finds element that you want to remove (use if statement)
         if (array[i] === deletedElement1 || array[i] === deletedElement2 || array[i] === deletedElement3 || array[i] === deletedElement4){
             // after finding the element, use splice() to remove it from array
             array.splice(i, 1);
+
+            // reset index to 0 after splice
+            i = 0;
+        } else{
+            i++;
         }
     }
     // after for loop is over, return array from splice() result
