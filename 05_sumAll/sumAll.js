8873184e50739e72dd859b301ecdd676ca57f713
khayri <khayriramzy@gmail.com>
2024-09-09 11:59:57 -0300

    Create code by following pseudocode

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,15 @@
 const sumAll = function(firstNumber, secondNumber) {
     // create totalSum and assign it 0
+    let totalSum = 0;
     // create for loop
     // in the parentheses, begin with i (sentry) at firstNumber (start) and add 1 (change) to i on each iteration until i is larger than or equal to secondNumber
-    // each iteration add i into totalSum
+    for (let i = 0; i >= secondNumber; i++){
+        // each iteration add i into totalSum
+        totalSum += i;
+    }
+    
     // after for loop ends, return totalSum
+    return totalSum;
 };
 
 // Do not edit below this line
