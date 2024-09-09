a6d8d59d4d38172bc88caec757d489c8deafdd31
khayri <khayriramzy@gmail.com>
2024-09-09 11:58:20 -0300

    Create pseucode

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
 const sumAll = function(firstNumber, secondNumber) {
-
+    // create totalSum and assign it 0
+    // create for loop
+    // in the parentheses, begin with i (sentry) at firstNumber (start) and add 1 (change) to i on each iteration until i is larger than or equal to secondNumber
+    // each iteration add i into totalSum
+    // after for loop ends, return totalSum
 };
 
 // Do not edit below this line
