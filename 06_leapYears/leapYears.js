bbbb157e247ebc2d9980483a31de7c9e2d4f2b03
khayri <khayriramzy@gmail.com>
2024-09-10 11:37:04 -0300

    Add more pseudocode and write code after

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -3,8 +3,27 @@ const leapYears = function(year) {
     // This means that the year 2000 was a leap year, although 1900 was not.
 
     // check if year is divisible by 4
-    // then check if it's and end-of-century year
-    // if it is, return true if divisible by 400
+    if (year % 4 === 0){
+        // then check if it's and end-of-century year
+        // if it is, return true if divisible by 400
+        if (year % 100 === 0 && year % 400 === 0){
+            return true;
+        }
+
+        // if not divisible by 400, return false
+        else if (year % 100 === 0 && year % 400 !== 0){
+            return false;
+        }
+
+        // if it is not an end-of-century year, return true
+        else if (year % 100 !== 0){
+            return true;
+        }
+    } else{
+        return false;
+    }
+    
+    
     // if not, return false
 };
 
