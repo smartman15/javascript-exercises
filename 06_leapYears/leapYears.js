bfea0fc247520cb7e05b67b145a87b0c63419a3d
khayri <khayriramzy@gmail.com>
2024-09-10 11:17:11 -0300

    Create if statements for if year is divisible by 4, and if year is divisible by 100 and 400

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
 const leapYears = function(year) {
-
+    // if year is divisible by 4, return true
+    if (year % 4 === 0){
+        return true;
+    // if year is divisible by 100 and 400, return true
+    } else if (year % 100 === 0 && year % 400 === 0){
+        return true;
+    }
 };
 
 // Do not edit below this line
