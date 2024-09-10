d02edebd48fbe86723aeb4ad5d9158b8e11baeff
khayri <khayriramzy@gmail.com>
2024-09-10 11:25:30 -0300

    Copy and paste what a leap year is

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,10 +1,15 @@
 const leapYears = function(year) {
+    // What is a leap year? To be a leap year, the year number must be divisible by four – except for end-of-century years, which must be divisible by 400. 
+    // This means that the year 2000 was a leap year, although 1900 was not.
+
     // if year is divisible by 4, return true
     if (year % 4 === 0){
         return true;
     // if year is divisible by 100 and 400, return true
     } else if (year % 100 === 0 && year % 400 === 0){
         return true;
+    } else{
+        return false;
     }
 };
 
