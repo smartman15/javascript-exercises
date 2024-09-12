1c978ddbbcbdf5fe6d79c7e5d1176667f52b4e5b
khayri <khayriramzy@gmail.com>
2024-09-12 12:21:39 -0300

    Create formula and round up to one decimal place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
 const convertToCelsius = function(temperatureFahrenheit) {
+  const formulaFahrenheit = (temperatureFahrenheit - 32) * (5/9);
 
+  // the toFixed function rounds formulaFahrenheit to 1 decimal place
+  // since toFixed turns the number into a string, 
+  // the Number() method is used to turn it back into a number
+  const roundedFahrenheit = Number(formulaFahrenheit.toFixed(1));
+  return roundedFahrenheit;
 };
 
 const convertToFahrenheit = function(temperatureCelcius) {
+  formulaCelcius = (temperatureCelcius * (9/5) + 32);
+  roundedCelcius = formulaCelcius.tofixed(1);
+  return roundedCelcius;
 };
 
 // Do not edit below this line
