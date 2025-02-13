ade4525ffa5dbc4bf3316ce9bc95f0dc5613b336
khayri <khayriramzy@gmail.com>
2025-02-13 08:46:57 -0300

    Use .reduce() for function 'multiply', remove test cases for 'multiply'

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -10,8 +10,8 @@ const sum = function(arr) {
 	return arr.reduce((accumulator, number) => accumulator + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((accum, number) => accum * number, 1);
 };
 
 const power = function() {
