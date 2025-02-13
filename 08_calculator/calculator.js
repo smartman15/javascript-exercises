686225959dac488958346b11dc835127964b67a9
khayri <khayriramzy@gmail.com>
2025-02-13 08:44:12 -0300

    Use .reduce() to create function sum, remove .skip from test cases

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,8 +6,8 @@ const subtract = function(a, b) {
 	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((accumulator, number) => accumulator + number, 0);
 };
 
 const multiply = function() {
