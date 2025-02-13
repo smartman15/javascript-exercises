2a46b5b822c359b85ac91ad6abcf07259a2b20fd
khayri <khayriramzy@gmail.com>
2025-02-13 12:01:21 -0300

    Fill pseudocode with code
    Only tested for the first test case

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,9 +1,18 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const wordLength = word.length;
+    let j = wordLength - 1;
+
     // start a for loop that goes from left to right
-        // start another for loop that goes from right to left
-            // check if the left and right are equal
-            // if all letters have been checked and equal, return true
-            // else return false
+    for(let i = 0; i < wordLength; i++){
+        // check if the left and right are equal
+        // if all letters have been checked and equal, return true
+        // else return false
+        if(word[i] !== word[j]) return false;
+        j--;
+            
+    }
+    return true;
+        
 };
 
 // Do not edit below this line
