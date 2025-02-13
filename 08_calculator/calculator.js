f1eeef00839ffc1a4210ef03b0d110cf895709bd
khayri <khayriramzy@gmail.com>
2025-02-13 08:52:55 -0300

    Create pseudocode for function 'factorial'

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -19,8 +19,12 @@ const power = function(a, b) {
 	
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  // create an empty array
+  // create for loop that starts from 1 and ends at a (if a is 1, add 1 into array)
+    // add i into array
+  
+  // use reduce() to multiply all numbers together and return value 
 };
 
 // Do not edit below this line
