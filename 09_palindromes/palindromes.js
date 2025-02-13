62f18c841f8516cd845ba6ab7a3925b33247a9dc
khayri <khayriramzy@gmail.com>
2025-02-13 11:42:10 -0300

    Create pseudocode

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
 const palindromes = function () {
-
+    // start a for loop that goes from left to right
+        // start another for loop that goes from right to left
+            // check if the left and right are equal
+            // if all letters have been checked and equal, return true
+            // else return false
 };
 
 // Do not edit below this line
