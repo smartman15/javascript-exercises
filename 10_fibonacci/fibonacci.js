6aab45481691d85291bd2637b57a358e2b6bc4a1
khayri <khayriramzy@gmail.com>
2025-02-16 12:14:20 -0300

    Add code

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,10 +1,26 @@
 const fibonacci = function(num) {
     let initial = [1, 1];
-
+    
     // create a variable to determine how many times the for loop should loop
+    let loop = num - 2; // 2 bc that's the length of the initial array
     // create the for loop
+    for(let i = 0; i < loop; i++){
+        let arrLen = initial.length;
         // use .reduce() to sum two preceding numbers and then add into the array
+        // make .reduce() start at index arrLength - 2
+        let value = initial.reduce(function(sum, number, index) {
+            if(index >= arrLen - 2){
+                return sum + number;
+            } else{
+                return 0;
+            }
+        }, 0);
+        // add value to initial
+        initial.push(value);
+    }
+        
     // return the number at index num-1
+    return initial[num-1];
 };
 
 // Do not edit below this line
