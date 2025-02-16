36a5c919a9ab7f41cf80b40bb7b3f0a70828abcd
khayri <khayriramzy@gmail.com>
2025-02-16 11:57:51 -0300

    Create pseudocode

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let initial = [1, 1];
 
+    // create a variable to determine how many times the for loop should loop
+    // create the for loop
+        // use .reduce() to sum two preceding numbers and then add into the array
+    // return the number at index num-1
 };
 
 // Do not edit below this line
