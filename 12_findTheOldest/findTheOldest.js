bbadd7a2b2c91673950481d3c8c4b59e329bfc43
khayri <khayriramzy@gmail.com>
2025-02-16 12:36:20 -0300

    Add pseudocode

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    // object structure
+    /*const people = [
+        {
+          name: "Carly",
+          yearOfBirth: 1942,
+          yearOfDeath: 1970,
+        },
+        {
+          name: "Ray",
+          yearOfBirth: 1962,
+          yearOfDeath: 2011,
+        },
+        {
+          name: "Jane",
+          yearOfBirth: 1912,
+          yearOfDeath: 1941,
+        },
+      ]*/
+    
+    // use sort() to determine who is the oldest
+    // in the sort() calculate age of the two people, sort from oldest to youngest
+    // return the object of the oldest person
 
 };
 
